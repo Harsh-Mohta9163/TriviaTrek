e564735b12e2a4dfb6fc24ed794b2933217ab2a1
Harsh Mohta <mohta.harsh9163@gmail.com>
2024-08-10 19:15:59 +0530

    added somethings

--- user/urls.py
+++ user/urls.py
@@ -4,12 +4,15 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('profile/update/', views.profile_update, name='profile_update'),
+
+
 ]
 
 if settings.DEBUG:
